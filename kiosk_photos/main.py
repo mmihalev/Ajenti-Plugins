defdeef948b16e12c9bbdf2a717789055a444d05
mmihalev <ermac@gbg.bg>
2016-02-25 02:02:49 +0200

    indent

--- kiosk_photos/main.py
+++ kiosk_photos/main.py
@@ -29,31 +29,31 @@ class SanickioskScreensaverPrefs (SectionPlugin):
 		self.binder.populate()
 
 		#all_vars = '\n'.join([k + '="' + str(v) + '"' for k,v in self.classconfig.iteritems()])
-        for k,v in self.classconfig.iteritems():
-            if k == 'photos_enable':
-                photos_enable = "X-GNOME-Autostart-enabled=%s" % str(v).lower()
+        	for k,v in self.classconfig.iteritems():
+			if k == 'photos_enable':
+				photos_enable = "X-GNOME-Autostart-enabled=%s" % str(v).lower()
         
-        if photos_enable == "X-GNOME-Autostart-enabled=true":
-            ajenti_config = "/etc/ajenti/config.json"
+        	if photos_enable == "X-GNOME-Autostart-enabled=true":
+			ajenti_config = "/etc/ajenti/config.json"
+		
+		#Disable Browser Mode
+            	subprocess.call(['sed', '-i', r's/\\"enable_browser\\": true/\\"enable_browser\\": false/g', ajenti_config])
+            	subprocess.call(['sed', '-i', r's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/g', "/home/kiosk/.config/autostart/2-browser.desktop"])
             
-            #Disable Browser Mode
-            subprocess.call(['sed', '-i', r's/\\"enable_browser\\": true/\\"enable_browser\\": false/g', ajenti_config])
-            subprocess.call(['sed', '-i', r's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/g', "/home/kiosk/.config/autostart/2-browser.desktop"])
+            	#Disable Video Mode
+            	subprocess.call(['sed', '-i', r's/\\"enable_videos\\": true/\\"enable_videos\\": false/g', ajenti_config])
+            	subprocess.call(['sed', '-i', r's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/g', "/home/kiosk/.config/autostart/2-videos.desktop"])
             
-            #Disable Video Mode
-            subprocess.call(['sed', '-i', r's/\\"enable_videos\\": true/\\"enable_videos\\": false/g', ajenti_config])
-            subprocess.call(['sed', '-i', r's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/g', "/home/kiosk/.config/autostart/2-videos.desktop"])
+        	cfg = ("[Desktop Entry]\n"
+		"Type=Application\n"
+            	"Exec=feh --recursive --quiet --randomize --full-screen --borderless --reload 300 --slideshow-delay 10 --no-menus --auto-zoom --hide-pointer /home/kiosk/Photos/\n"
+            	"Hidden=false\n"
+            	"NoDisplay=false\n"
+            	"%s\n"
+            	"Name[en_US]=2-photos\n"
+            	"Name=2-photos\n"
+            	"Comment[en_US]=\n"
+            	"Comment=") % (photos_enable)
             
-        cfg = ("[Desktop Entry]\n"
-            "Type=Application\n"
-            "Exec=feh --recursive --quiet --randomize --full-screen --borderless --reload 300 --slideshow-delay 10 --no-menus --auto-zoom --hide-pointer /home/kiosk/Photos/\n"
-            "Hidden=false\n"
-            "NoDisplay=false\n"
-            "%s\n"
-            "Name[en_US]=2-photos\n"
-            "Name=2-photos\n"
-            "Comment[en_US]=\n"
-            "Comment=") % (photos_enable)
-            
-        open('/home/kiosk/.config/autostart/2-photos.desktop', 'w').write(cfg) #save
+        	open('/home/kiosk/.config/autostart/2-photos.desktop', 'w').write(cfg) #save
 
